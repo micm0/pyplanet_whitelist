50fba801b899c4c1e6e0e0ec7876df07192557d3
El Mezrichi Monir <monir.elmezrichi@hotmail.com>
2021-05-01 03:22:30 +0200

    wl based on logins instead of players obj + new GUI + some fixes

--- whitelist/__init__.py
+++ whitelist/__init__.py
@@ -1,6 +1,7 @@
 from pyplanet.apps.config import AppConfig
 from pyplanet.apps.core.maniaplanet import callbacks as mp_signals
 from pyplanet.contrib.command import Command
+from .view import WhiteListView, WhiteListWidget
 
 class Whitelist(AppConfig):
     name = 'whitelist'
@@ -10,12 +11,14 @@ class Whitelist(AppConfig):
         super().__init__(*args, **kwargs)
         self.whitelist = []
         self.active = False
+        self.widget = WhiteListWidget(self)
 
     async def on_start(self):
         await self.instance.permission_manager.register('admin', 'Administer the whitelist', app=self, min_level=2)
         await self.instance.command_manager.register(
-            Command(command='add', namespace='wl', target=self.add_player, perms='whitelist:admin', admin=True, description='Add a player to the local whitelist with his login or nickname.').add_param(name='player', required=True),
-            Command(command='remove', namespace='wl', target=self.remove_player, perms='whitelist:admin', admin=True, description='Remove a player from the local whitelist with his login or nickname.').add_param(name='player', required=True),
+            Command(command='add', namespace='wl', target=self.add_player_from_server, perms='whitelist:admin', admin=True, description='Add a player login to the local whitelist with his login or nickname.').add_param(name='player', required=True),
+            Command(command='addlogin', namespace='wl', target=self.add_player_login, perms='whitelist:admin', admin=True, description='Add a player login to the local whitelist.').add_param(name='player_login', required=True),
+            Command(command='remove', namespace='wl', target=self.remove_player_login, perms='whitelist:admin', admin=True, description='Remove a player login from the local whitelist.').add_param(name='player', required=True),
             Command(command='current', namespace='wl', target=self.add_current_players, perms='whitelist:admin', admin=True, description='Create a new the local whitelist with all current connected players.'),
             Command(command='clear', namespace='wl', target=self.clear, perms='whitelist:admin', admin=True, description='Clear the local whitelist.'),
             Command(command='show', namespace='wl', target=self.show, perms='whitelist:admin', admin=True, description='Show the players from the local whitelist in the chat.'),
@@ -23,21 +26,30 @@ class Whitelist(AppConfig):
             Command(command='off', namespace='wl', target=self.deactivate, perms='whitelist:admin', admin=True, description='Deactivate the local whitelist.'),
             Command(command='help', namespace='wl', target=self.show_commands, perms='whitelist:admin', admin=True, description='Show available whitelist commands in the chat.'),
             Command(command='status', namespace='wl', target=self.get_status, perms='whitelist:admin', admin=True, description='Show the status of the whitelist(on or off) in the chat'),
+            Command(command='whitelist', target=self.show_whitelist_window, perms='whitelist:admin', admin=True, description='Show the whitelist window'),
         )
         self.context.signals.listen(mp_signals.player.player_connect, self.player_connect)
+
+        # Display widget for admin roles 
+        for player in self.instance.player_manager.online:
+            if player.level > 0:
+                await self.widget.display([player.login])
        
 
     async def player_connect(self, player, *args, **kwargs):
+        # Check if player(not admin) is in the whitelist and kick him if not
         if self.active:
             if player.level == 0:
-                if self.whitelist != []:
-                    if player not in self.whitelist:
-                        await self.instance.gbx('Kick', player.login, 'You are not in the whitelist')
-                        await self.instance.chat(f'{player.nickname} $ff0is not whitelisted!')
-                    else:
-                        await self.instance.chat(f'{player.nickname} $ff0is whitelisted!')
+                if player.login not in self.whitelist:
+                    await self.instance.gbx('Kick', player.login, 'You are not in the whitelist')
+                    await self.instance.chat(f'{player.nickname} $ff0is not whitelisted!')
+                else:
+                    await self.instance.chat(f'{player.nickname} $ff0is whitelisted!')
             else:
                 await self.instance.chat(f'{player.nickname} $ff0is whitelisted!')
+        # Display widget for admin roles        
+        if player.level > 0:
+            await self.widget.display([player.login])
 
     async def activate(self, player, **kwargs):
         if(self.active):
@@ -45,8 +57,11 @@ class Whitelist(AppConfig):
         else:
             self.active = True
             await self.instance.chat('$ff0Whitelist Activated')
-            for player_online in self.instance.player_manager.online:
-                if player_online not in self.whitelist and player_online.level == 0:
+            await self.kick_players_not_in_wl(player)
+    
+    async def kick_players_not_in_wl(self, player, **kwargs):
+        for player_online in self.instance.player_manager.online:
+                if player_online.login not in self.whitelist and player_online.level == 0:
                     await self.instance.gbx('Kick', player_online.login, 'You are not in the whitelist')
     
     async def deactivate(self, player, **kwargs):
@@ -65,12 +80,8 @@ class Whitelist(AppConfig):
     async def find_player_by_login_or_nickname(self, player_login_or_nickname):
         player_found = [p for p in self.instance.player_manager.online if p.nickname == player_login_or_nickname or p.login == player_login_or_nickname]
         return player_found
-    
-    async def find_player_by_login_or_nickname_in_wl(self, player_login_or_nickname):
-        player_found = [p for p in self.whitelist if p.nickname == player_login_or_nickname or p.login == player_login_or_nickname]
-        return player_found
 
-    async def add_player(self, player, data = None, **kwargs):
+    async def add_player_from_server(self, player, data = None, **kwargs):
         player_found = await self.find_player_by_login_or_nickname(data.player)
         if not len(player_found) == 1:
             await self.instance.chat(f'{data.player} $f00is not found!', player.login)
@@ -80,20 +91,40 @@ class Whitelist(AppConfig):
                 await self.instance.chat(f'{player_found[0].nickname} $0C0added to the local whitelist!')
             else:
                 await self.instance.chat(f'{player_found[0].nickname} $f00is already whitelisted locally!', player.login)
+    
+    async def add_player_login(self, player, data, **kwargs):
+        if data.player_login not in self.whitelist:
+            self.whitelist.append(data.player_login)
+            await self.instance.chat(f'{data.player_login} $0C0added to the local whitelist!')
+        else:
+            await self.instance.chat(f'{data.player_login} $f00is already whitelisted locally!', player.login)
 
-    async def remove_player(self, player, data = None, **kwargs):
-        player_found = await self.find_player_by_login_or_nickname_in_wl(data.player)
-        if not len(player_found) == 1:
+    async def add_players_from_view(self, player, logins, **kwargs):
+        logins = logins.replace(' ', '')
+        logins_list = logins.split(",")
+        for login in logins_list:
+            if login != "":
+                if login not in self.whitelist:
+                    self.whitelist.append(login)
+                    await self.instance.chat(f'{login} $0C0added to the local whitelist!')
+                else:
+                    await self.instance.chat(f'{login} $f00is already whitelisted locally!', player.login)
+
+    async def remove_player_login(self, player, data = None, **kwargs):
+        if data.player not in self.whitelist:
             await self.instance.chat(f'{data.player} $f00is not on local Whitelist, cannot be removed!', player.login) 
         else:
-            self.whitelist.remove(player_found[0])
+            self.whitelist.remove(data.player)
+            if self.active:
+                target_player = await self.instance.player_manager.get_player(login=data['login'], lock=False)
+                if target_player.level == 0:
+                    await self.app.instance.gbx('Kick', target_player.login, 'You are not in the whitelist')
             await self.instance.chat(f'{data.player} $0C0removed from the local whitelist!')
 
     async def add_current_players(self, **kwargs): 
         self.whitelist = []
         for player_online in self.instance.player_manager.online:
-            if player_online not in self.whitelist:
-                self.whitelist.append(player_online)
+                self.whitelist.append(player_online.login)
         await self.instance.chat('$ff0Local Whitelist with current Players and Spectators created!')
 
     async def show(self, player, **kwargs):
@@ -103,18 +134,24 @@ class Whitelist(AppConfig):
         else:
             for i, player_in_wl in enumerate(self.whitelist):
                 if i == len(self.whitelist) - 1:
-                    whitelist_as_string += f'{player_in_wl.nickname}'
+                    whitelist_as_string += f'{player_in_wl}'
                 else:
-                    whitelist_as_string += f'{player_in_wl.nickname}, '
+                    whitelist_as_string += f'{player_in_wl}, '
             await self.instance.chat(f'$ff0Players in the local whitelist: $fff{whitelist_as_string}', player.login)
 
-    async def clear(self, **kwargs):
+    async def clear(self, player, **kwargs):
         self.whitelist = []
+        if self.active:
+            await self.kick_players_not_in_wl(player)
         await self.instance.chat('$0C0Local whitelist cleared!')
 
     async def show_commands(self, player, **kwargs):
         commands_string = ""
-        commands = ['//wl add $iplayer$z$s | ','//wl remove $iplayer$z$s | ', '//wl current | ', '//wl clear | ', '//wl show | ', '//wl on | ', '//wl off | ', '//wl help']
+        commands = ['//wl whitelist | ', '//wl add $iplayernickname$z$s | ', '//wl addlogin $iplayerlogin$z$s | ', '//wl remove $iplayer$z$s | ', '//wl current | ', '//wl clear | ', '//wl show | ', '//wl on | ', '//wl off | ', '//wl status | ', '//wl help']
         await self.instance.chat(commands_string.join(commands), player.login)
+    
+    async def show_whitelist_window(self, player, *args, **kwargs):
+        view = WhiteListView(self, self.whitelist)
+        await view.show(player)
 
     
---
+++ whitelist/view.py
@@ -0,0 +1,242 @@
+import asyncio
+
+from pyplanet.views.generics.list import ManualListView
+from pyplanet.views import TemplateView
+from pyplanet.views.generics.alert import show_alert, ask_confirmation
+from pyplanet.contrib.player.exceptions import PlayerNotFound
+
+class WhiteListView(ManualListView):
+    title = 'Whitelist'
+    icon_style = 'Icons128x128_1'
+    icon_substyle = 'ChallengeAuthor'
+
+    def __init__(self, app, whitelist):
+        """
+        Initiate the Whitelist List View
+        """
+        super().__init__(self)
+        self.app = app
+        self.fields = self._create_fields()
+        self.whitelist = whitelist
+        self.manager = app.context.ui
+        self.actions = self._create_actions()
+        self.child = None
+
+    async def show(self, player):
+        await self.display(player=player)
+
+    def _create_fields(self):
+        return [
+            {
+                'name': 'Login',
+                'index': 'login',
+                'sorting': True,
+                'searching': True,
+                'width': 90,
+                'type': 'label',
+            },
+            {
+                'name': 'Nickname',
+                'index': 'nickname',
+                'sorting': True,
+                'searching': True,
+                'width': 90,
+                'type': 'label',
+            },
+        ]
+    
+    async def get_buttons(self):
+        buttons = [
+            {
+                'title': ' Clear',
+                'width': 15,
+                'action': self.action_clear
+            },
+            {
+                'title': '  Add New Player(s)',
+                'width': 30,
+                'action': self.action_new_players
+            },
+            {
+                'title': '  New List With Current Players',
+                'width': 50,
+                'action': self.action_current
+            },
+        ]
+        if self.app.active:
+            buttons.append({
+                'title': '$F00 $fffDeactivate',
+                'width': 20,
+                'action': self.action_deactivate
+            })
+        else:
+            buttons.append({
+                'title': '$0C0 $fffActivate',
+                'width': 20,
+                'action': self.action_activate
+            },)
+        return buttons
+    
+    def _create_actions(self):
+        return [
+            dict(
+                name='delete',
+                action=self.delete,
+                text='&#xF1F8;',
+                textsize=1.2,
+                safe=True,
+                type='label',
+                order=0,
+                require_confirm=False,
+            )
+        ]
+    
+    async def get_data(self):
+        items = []
+        for player_login in self.whitelist:
+            try:
+                target_player = await self.app.instance.player_manager.get_player(login=player_login,lock=False)
+                items.append({
+                    'login': player_login,
+                    'nickname': target_player.nickname,
+                })
+            except PlayerNotFound:
+                items.append({
+                    'login': player_login,
+                    'nickname': 'Unknown',
+                })            
+        return items
+    
+    async def action_current(self, player, values, **kwargs):
+        await self.app.add_current_players()
+        await self.get_data()
+        await self.close(player=player)
+    
+    async def action_clear(self, player, values, **kwargs):
+        cancel = bool(
+            await ask_confirmation(player, 'Do you really want to clear the whitelist ? If the whitelist is active, it will kick all the players without an admin role')
+        )
+        if not cancel:
+            await self.app.clear(player)
+            await self.close(player=player)
+
+    async def action_activate(self, player, values, **kwargs):
+        cancel = bool(
+            await ask_confirmation(player, 'Do you really want to activate the whitelist ? That will kick all non admin players who are not in the whitelist')
+        )
+        if not cancel:
+            await self.app.activate(player)
+            await self.display(player=player)
+
+    async def action_deactivate(self, player, values, **kwargs):
+        await self.app.deactivate(player)
+        await self.display(player=player)
+
+    async def delete(self, player, values, data, **kwargs):
+        cancel = bool(
+            await ask_confirmation(player, 'Do you really want to delete this player {} | {}? If the whitelist is active, it will kick the player if he don\'t have an admin role'.format(data['login'], data['nickname']))
+        )
+        if not cancel:
+            self.app.whitelist.remove(data['login'])
+            if self.app.active:
+                target_player = await self.app.instance.player_manager.get_player(login=data['login'], lock=False)
+                if target_player.level == 0:
+                    await self.app.instance.gbx('Kick', target_player.login, 'You are not in the whitelist')
+            await self.display(player=player)
+    
+    async def action_new_players(self, player, values, **kwargs):
+        if self.child:
+            return
+
+        self.child = NewPlayersView(self, player, self.app)
+        await self.child.display()
+        await self.child.wait_for_response()
+        await self.child.destroy()
+        await self.display(player)  # refresh.
+        self.child = None
+
+class WhiteListWidget(TemplateView):
+    template_name = 'whitelist/whitelist.xml'
+
+    def __init__(self, app, *args, **kwargs):
+        """
+        Initiate the Whitelist Button
+        """
+        super().__init__(*args, **kwargs)
+        self.app = app
+        self.manager = self.app.context.ui
+        self.subscribe('bar_button_display', self.action_open_wl_view)
+
+    async def action_open_wl_view(self, player, *args, **kwargs):
+        return await self.app.instance.command_manager.execute(player, '//whitelist')
+
+class NewPlayersView(TemplateView):
+    """
+    View to add new logins.
+    """
+    template_name = 'whitelist/new_players.xml'
+
+    def __init__(self, parent, player, app):
+        """
+        Initiate child create view.
+
+        :param parent: Parent view.
+        :param player: Player instance.
+        :param app: app instance.
+        :type parent: pyplanet.view.base.View
+        :type player: pyplanet.apps.core.maniaplanet.models.player.Player
+        :type folder_manager: pyplanet.apps.contrib.jukebox.folders.FolderManager
+        """
+        super().__init__(parent.manager)
+
+        self.parent = parent
+        self.player = player
+        self.app = app
+
+        self.response_future = asyncio.Future()
+
+        self.subscribe('button_close', self.close)
+        self.subscribe('button_save', self.save)
+        self.subscribe('button_cancel', self.close)
+
+    async def display(self, **kwargs):
+        await super().display(player_logins=[self.player.login])
+
+    async def close(self, player, *args, **kwargs):
+        """
+        Close the link for a specific player. Will hide manialink and destroy data for player specific to save memory.
+
+        :param player: Player model instance.
+        :type player: pyplanet.apps.core.maniaplanet.models.Player
+        """
+        if self.player_data and player.login in self.player_data:
+            del self.player_data[player.login]
+        await self.hide(player_logins=[player.login])
+
+        self.response_future.set_result(None)
+        self.response_future.done()
+
+    async def wait_for_response(self):
+        return await self.response_future
+
+    async def save(self, player, action, values, *args, **kwargs):
+        """
+        Save action.
+
+        :param player: Player instance
+        :param action: Action label
+        :param values: Values from manialink
+        :param args: *
+        :param kwargs: **
+        :type player: pyplanet.apps.core.maniaplanet.models.Player
+        """
+
+        # Get logins from input entry
+        logins = values['player_login']
+
+        # Add new player
+        await self.app.add_players_from_view(player, logins)
+
+        # Return response.
+        self.response_future.set_result(None)
+        self.response_future.done()
